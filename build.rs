9ca12335614683f4abfc0dc6ff2101b594013d80
Tomas Carnecky <tomc@caurea.org>
2025-11-14 23:53:36 +0100

    format

--- build.rs
+++ build.rs
@@ -2,9 +2,6 @@ use std::io::Result;
 fn main() -> Result<()> {
     tonic_prost_build::configure()
         .build_server(false)
-        .compile_protos(
-            &["grpc/router.proto", "grpc/lightning.proto"],
-            &["grpc/"],
-        )?;
+        .compile_protos(&["grpc/router.proto", "grpc/lightning.proto"], &["grpc/"])?;
     Ok(())
 }
