621ee257db3bde6f7379b52b189a844016695f59
Tomas Carnecky <tomc@caurea.org>
2026-06-30 14:43:18 +0200

    Drop oxalica/rust-overlay input

--- flake.nix
+++ flake.nix
@@ -1,65 +1,28 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    flake-utils.url = "github:numtide/flake-utils";
-
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    systems.url = "github:nix-systems/default";
   };
 
   outputs =
     {
       nixpkgs,
-      flake-utils,
-      rust-overlay,
+      systems,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        rustToolchain = pkgs.rust-bin.stable.latest.default;
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            rustToolchain
-            rust-analyzer
-            protobuf
-            postgresql
-            libiconv
-          ];
+    let
+      forAllSystems =
+        function: nixpkgs.lib.genAttrs (import systems) (system: function nixpkgs.legacyPackages.${system});
 
-          shellHook = ''
-            # https://github.com/NixOS/nixpkgs/issues/376958#issuecomment-3471021813
-            unset DEVELOPER_DIR
-          '';
-        };
-
-        devShells.workflow = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            rustToolchain
-            rust-analyzer
-            protobuf
-            libiconv
-            openssl
-            pkg-config
-          ];
-        };
-
-        packages.default = pkgs.rustPlatform.buildRustPackage {
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        default = pkgs.rustPlatform.buildRustPackage {
           pname = "sarne";
           version = "0.1.0";
+
           src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
+          cargoLock.lockFile = ./Cargo.lock;
 
           buildInputs = with pkgs; [
             iconv
@@ -71,6 +34,42 @@
             pkg-config
           ];
         };
-      }
-    );
+      });
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            rustfmt
+            clippy
+            rust-analyzer
+
+            protobuf
+            postgresql
+            libiconv
+          ];
+
+          shellHook = ''
+            # https://github.com/NixOS/nixpkgs/issues/376958#issuecomment-3471021813
+            unset DEVELOPER_DIR
+          '';
+        };
+
+        workflow = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            rustfmt
+            clippy
+            rust-analyzer
+
+            protobuf
+            libiconv
+            openssl
+            pkg-config
+          ];
+        };
+      });
+    };
 }
